05a7504e94f465cfbf6ea1d9e5012109c85266bc
prkhr-agrwl <aprakhar51@gmai.com>
2019-06-28 15:20:01 +0530

    v2 can add comments

--- app.js
+++ app.js
@@ -1,38 +1,111 @@
-var express = require("express");
-var app= express();
-var bodyparser = require("body-parser");
 
+var express 	= require("express"), 
+	app     	= express(), 
+	bodyparser 	= require("body-parser"),
+    mongoose 	= require("mongoose"),
+	Campground  = require("./models/campground"),
+	Comment     = require("./models/comments"),
+	// User        = require("./models/user"),
+	seedDB		= require("./seeds");
+
+mongoose.connect("mongodb://localhost/yelpcamp_Data",{ useNewUrlParser: true });
 app.use(bodyparser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
+seedDB();
 
-var campgrounds = [
-		{name:"Salmon Creek", image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80.jpg"},
-		{name:"Granite Hill", image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80.jpg"},
-		{name:"Ventura Ranch", image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80.jpg"},
-		{name:"Lake Rudolph", image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80.jpg"},
-		{name:"LakeDale Resort", image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80.jpg"}
-	];
+// Campground.create(
+// 	{
+// 		name:"Granite Hill",
+// 		image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80.jpg",
+// 		description: "This is a huge Granite Hill. With Granite."
+// 	},function(err,camp){
+// 		if(err){
+// 			console.log(err);
+// 		}
+// 		else{
+// 			console.log(camp);
+// 		}
+// });
 
+//landing
 app.get("/",function(req,res){
-	res.render("landing");
-});
-
-app.get("/campgrounds", function(req, res){
-	res.render("campgrounds", {campgrounds:campgrounds});
-});
-
-app.post("/campgrounds", function(req, res){
-	var name = req.body.name;
-	var image = req.body.image;
-	var newCamp = {name:name, image:image};
-	campgrounds.push(newCamp);
-	// console.log(name+" "+image);
-
 	res.redirect("/campgrounds");
 });
 
+//list all camps
+app.get("/campgrounds", function(req, res){
+	Campground.find({}, function(err, camp){
+		if(err) console.log(err);
+		else {
+			res.render("campgrounds/index", {campgrounds:camp});
+		}
+	});
+});
+
+//to add a new camp
+app.post("/campgrounds", function(req, res){
+	var name = req.body.name;
+	var image = req.body.image;
+	var description = req.body.description;
+	var newCamp = {name:name, image:image, description:description};
+	
+	Campground.create(
+	newCamp,function(err,camp){
+		if(err){
+			console.log(err);
+		}
+		else{
+			res.redirect("/campgrounds");
+		}
+	});
+	//console.log(name+" "+image);
+});
+
+//new camp(form only)
 app.get("/campgrounds/new", function(req, res){
-	res.render("new");
+	res.render("campgrounds/new");
+});
+
+//show
+app.get("/campgrounds/:id", function(req, res){
+	Campground.findById(req.params.id).populate("comments").exec(function(err, campbyID){
+		if(err) console.log(err);
+		else{
+			// console.log(campbyID);
+			res.render("campgrounds/show",{campground:campbyID});
+		}
+	})
+});
+
+
+//========COMMENT ROUTES=======//
+
+
+app.get("/campgrounds/:id/comments/new", function(req, res){
+	Campground.findById(req.params.id, function(err, campbyID){
+		if(err) console.log(err);
+		else{
+			// console.log(campbyID);
+			res.render("comments/new",{campground:campbyID});
+		}
+	});
+});
+
+app.post("/campgrounds/:id/comments/", function(req, res){
+	Campground.findById(req.params.id, function(err, campbyID){
+		if(err) console.log(err);
+		else{
+			// console.log(campbyID);
+			Comment.create(req.body.comment, function(err, comment){
+				if(err) console.log(err);
+				else{
+					campbyID.comments.push(comment);
+					campbyID.save();
+					res.redirect("/campgrounds/"+req.params.id);
+				}
+			})
+		}
+	});
 });
 
 app.listen(3000, function(){
